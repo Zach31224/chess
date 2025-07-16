04b10ac1b094704dcedb09d17b569b5123822e36
Zachary Sandberg <zsandber@byu.edu>
2025-07-15 22:27:58 -0600

    Game handler and service created and updated

---
+++ server/src/main/java/server/GameHandler.java
@@ -0,0 +1,14 @@
+package server;
+
+import service.GameService;
+
+public class GameHandler {
+
+    private final GameService service;
+
+    public GameHandler(GameService service) {
+        this.service = service;
+    }
+
+    // I'll probably have more to add here later
+}
---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,16 @@
+package service;
+
+import dataAccess.GameDAO;
+
+public class GameService {
+
+    GameDAO gameDAO;
+
+    public GameService(GameDAO gameDAO) {
+        this.gameDAO = gameDAO;
+    }
+
+    public void clear() {
+        gameDAO.clear();
+    }
+}
