c49f4bf2725f352d96568f44d760dd4e07cbe609
Zachary Sandberg <zsandber@byu.edu>
2025-07-15 20:57:34 -0600

    Updated data models and MemoryUserDAO again.

--- server/src/main/java/dataAccess/MemoryUserDAO.java
+++ server/src/main/java/dataAccess/MemoryUserDAO.java
@@ -13,6 +13,48 @@ public class MemoryUserDAO implements UserDAO {
         users = new HashSet<>(16);
     }
 
+    @Override
+    public UserData getUser(String username) {
+        return users.stream()
+                .filter(user -> user.username().equals(username))
+                .findFirst()
+                .orElse(null);
+    }
 
+    @Override
+    public void createUser(String username, String password, String email) {
+        users.add(new UserData(username, password, email));
+    }
+
+    @Override
+    public boolean authenticateUser(String username, String password) {
+        return users.stream()
+                .anyMatch(user -> user.username().equals(username) &&
+                        user.password().equals(password));
+    }
+
+    @Override
+    public void clear() {
+        users.clear();
+    }
+
+    @Override
+    public UserData findUserByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public void addUser(String username, String password, String email) {
+
+    }
+
+    @Override
+    public boolean validateCredentials(String username, String password) {
+        return false;
+    }
+
+    @Override
+    public void reset() {
+
+    }
 }
-
